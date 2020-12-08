0b21be728de8e803fc73185e9bf59c7438c1ea9c
Souvik Ghosh <42302494+SouvikSSG@users.noreply.github.com>
2020-12-08 22:14:24 +0530

    Merge sort added

---
+++ Sorting Techniques/merge sort.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void Merge(int A[],int l,int mid,int h)
+{
+    int i=l,j=mid+1,k=l;
+    int B[50];
+    
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+        {
+            B[k++]=A[i++];
+        }
+        
+        else
+        {
+            B[k++]=A[j++];
+        
+        }
+    }
+    for(;i<=mid;i++)
+    {
+        B[k++]=A[i];
+    }
+    for(;j<=h;j++)
+    {
+        B[k++]=A[j];
+    }
+    for(i=l;i<=h;i++)
+    {
+        A[i]=B[i];
+    }
+}
+
+void MergeSort(int A[],int l,int h)
+{
+    int mid;
+    if(l<h)
+    {
+        mid=(l+h)/2;
+        MergeSort(A,l,mid);
+        MergeSort(A,mid+1,h);
+        Merge(A,l,mid,h);
+    }
+}
+
+
+int main()
+{
+    int *p, num ,i;
+    
+    std:: cout<<"Enter the number of elements to sort: ";
+    std:: cin>>num;
+    p= new int [num];
+    std:: cout<<"\nPut the numbers: ";
+    for(i=0; i<num; i++)
+    {
+        std:: cin>> *(p+i); 
+    }
+    MergeSort(p, 0, num-1);
+
+    std:: cout<<std:: endl;
+    for(i=0; i<num; i++)
+    {
+        std:: cout<<*(p+i)<<"\t";
+    }
+    delete [] p;
+    return 0;
+}
